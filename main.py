7d13eddba6fc72e3f8a1b74165cd40486ffabb48
Shruti Shukla <142007162+911-shruti@users.noreply.github.com>
2025-07-26 11:09:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import json
+import os
 from datetime import datetime
 import uuid  # importing to help generate unique receipt no.
 
 items = []
+
+# defining file name
+filename = "receipts.json"
+
+# checking if path exists
+if os.path.exists(filename):
+    with open(filename, "r") as file:
+        all_receipts = json.load(file) #load existing data
+else:
+    all_receipts = {}  # Start with an empty dictionary
+
+
 while True:
     # taking input from user about item purchase
     print("\nEnter item details:")
@@ -31,6 +45,8 @@ while True:
 # adding current date and time to receipt
 now = datetime.now()
 date_str = now.strftime("%Y-%m-%d %H:%M:%S")
+# strftime method is used to format a datetime object into 
+# a string representation based on a specified format
 receipt_no = "RCPT" + str(uuid.uuid4())[:8].upper()
 
 # printing the receipt
@@ -44,12 +60,30 @@ print("-" * 40)
 
 # adding up the total for each item
 subtotal = 0
-for item in items:
+for it in items:
     print("{:<12} {:>4} {:>7.2f} {:>5.1f} {:>9.2f}".format(
-        item["name"], item["qty"], item["price"], item["tax_rate"], item["total"]))
-    subtotal += item["total"]
+        it["name"], it["qty"], it["price"], it["tax_rate"], it["total"]))
+    subtotal += it["total"]
 
 print("-" * 40)
 print("{:<28} {:>8.2f}".format("Grand Total:", subtotal))
 print("=" * 40)
 print("Thanks for shopping with XYZ General Store!")
+
+# creating the receipt data dictionary
+receipt_data = {
+    "date": date_str,
+    "receipt_no": receipt_no,
+    "items": items,
+    "total": subtotal
+}
+ 
+# Step 4: Add new receipt using receipt_no as key
+all_receipts[receipt_no] = receipt_data
+
+# Step 5: Save back to file
+with open(filename, "w") as file:
+    json.dump(all_receipts, file, indent=4)
+    # ident =4 says print with identation of 4 spaces
+
+print("✅ Receipt saved successfully to 'receipts.json'")
